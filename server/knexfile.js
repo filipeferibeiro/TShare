5de91f079cc88b4c76ef347f7495a8b5f3e6bf87
Marlon Costa <marlon.costa@academico.ifpb.edu.br>
2020-12-01 00:48:06 -0300

    Update knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,12 +5,12 @@ module.exports = {
     client: 'mysql',
     version: '5.7',
     connection: {
-      host : 'localhost',
-      port: 3306,
-      user : 'root',
-      password : 'm2r8m0c2',
-      database : 'development',
-      charset: 'utf8'
+      host : process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DATABASE,
+      charset: process.env.CHARSET
     },
     migrations: {
       directory: __dirname + '/migrations',
