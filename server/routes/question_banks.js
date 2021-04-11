988357066783ae063a5c366a0a85b2272bf7f977
Marlon Costa <marlon.costa@academico.ifpb.edu.br>
2021-04-11 16:52:59 -0300

    Correção no questionBanksCheck

--- server/routes/question_banks.js
+++ server/routes/question_banks.js
@@ -6,6 +6,6 @@ const utils = require('../utils/jwt-verify')
 router.get('/questionBanks', controller.getBanks)
 router.post('/questionBanks', utils.verifyJWT, controller.post)
 router.get('/questionBanksCheck', controller.checkBanksOfQuestion)
-router.get('/addQuestionToBank', utils.verifyJWT, controller.addQuestionToBank)
+router.post('/addQuestionToBank', utils.verifyJWT, controller.addQuestionToBank)
 router.get('/getBankQuestions', utils.verifyJWT, controller.getBankQuestions)
 module.exports = router;
