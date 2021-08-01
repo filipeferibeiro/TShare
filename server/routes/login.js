949bf68d911af9e5891bdc7c9a85e61082a69973
Marlon Costa <marlon.costa@academico.ifpb.edu.br>
2021-07-31 22:14:51 -0300

    Separação dos projetos

--- server/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const loginController = require('../controllers/login_controller')
-
-/* GET users listing. */
-router.post('/login', loginController.login)
-router.post('/logout', loginController.logout)
-router.get('/checkToken', loginController.checkToken)
-module.exports = router;
